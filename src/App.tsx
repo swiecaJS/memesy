d75b40cc42beaeb3ca9e04c55d7d302a016754b1
swiecaJS <karol.swieca@netguru.co>
2019-05-12 17:04:42 +0200

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const Index: React.FC = () => {
+  return (
+    <main className="container mx-auto text-center">
+      <div className="text-base pt-4">
+        <Link className="" to="/create">
+          Create memes 🛠
+        </Link>
+        <Link className="" to="/yours">
+          Your memes ❤️
+        </Link>
+      </div>
+    </main>
+  );
+};
+const Create: React.FC = () => {
+  return (
+    <main>
+      <h1>Create Memes </h1>
+    </main>
+  );
+};
+const Yours: React.FC = () => {
+  return (
+    <main>
+      <h1>Yours memes</h1>
+    </main>
+  );
+};
+
+const Root: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <header className="bg-purple-darker m-6 p-6 rounded shadow-lg">
+        <h1 className="text-white text-3xl">Memesy</h1>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link
+              className="bg-purple-darker text-white p-3 rounded no-underline hover:bg-purple-dark ml-2"
+              to="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="bg-purple-darker text-white p-3 rounded no-underline hover:bg-purple-dark ml-2"
+              to="/create"
+            >
+              Create
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Route path="/" component={Index} exact />
+      <Route path="/create" component={Create} />
+      <Route path="/yours" component={Yours} />
+    </BrowserRouter>
+  );
+};
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return <Root />;
+};
 
 export default App;
